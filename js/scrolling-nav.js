1e7c02e430215fd5797972825583092fad3762df
Micah Schehl <micah@micahs-rmbp.local>
2015-03-30 00:01:58 -0400

    Added Quick Start and other pages
    Added Quick Start, Enhancement Docs, and reworked releases page

---
+++ js/scrolling-nav.js
@@ -0,0 +1,11 @@
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - $(".navbar").height()
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
